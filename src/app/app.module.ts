3dc4d112eb331f7d13ad36a1d1fb8da83d41e591
Max Fröhlich <maxbruchmann@gmail.com>
2020-07-25 12:58:15 +0200

    setup todos component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
