a2432da72127121aee854038a8ac1a08f6b24b58
Max Fröhlich <maxbruchmann@gmail.com>
2020-07-25 15:38:51 +0200

    catch error and model state

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodosService } from '../todos.service';
+import { catchError, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -7,7 +9,20 @@ import { TodosService } from '../todos.service';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-  todos$ = this.todosService.findAll();
+  todosState$ = this.todosService.findAll().pipe(
+    map((result) => {
+      return {
+        todos: result,
+        error: null,
+      };
+    }),
+    catchError((e: Error) => {
+      return of({
+        todos: [],
+        error: e.message,
+      });
+    })
+  );
 
   constructor(private readonly todosService: TodosService) {}
 
